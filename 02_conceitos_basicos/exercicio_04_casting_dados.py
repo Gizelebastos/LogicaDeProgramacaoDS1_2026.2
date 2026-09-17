de7fbd8cead5f13689ab1340667649f47ad37e4a
Gizelebastos <gizeleoliveirabastos867@gmail.com>
2026-09-17 14:35:19 -0300

    casting de dados.py

--- 02_conceitos_basicos/exercicio_04_casting_dados.py
+++ 02_conceitos_basicos/exercicio_04_casting_dados.py
@@ -10,3 +10,7 @@ Imprima a idade calculada com uma mensagem personalizada.
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+print("Bem-vindo ao nosso programa!")
+ano_de_nascimento = int(input("digite a ano de nascimento: "))
+idade_em_2026 = (2026 - ano_de_nascimento)
+print(f"a idade que a pessoa completara ate final de 2026 é de {idade_em_2026} anos")
---
+++ 02_conceitos_basicos/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+ano_de_nascimento = int(input("digite a ano de nascimento (em formato de texto):"))
