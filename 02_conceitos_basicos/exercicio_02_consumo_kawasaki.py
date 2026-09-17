181225114cb8102c9a233891ebe032265d11ac6e
Gizelebastos <gizeleoliveirabastos867@gmail.com>
2026-09-17 13:46:10 -0300

    Consumo Kawasaki.py

--- 02_conceitos_basicos/exercicio_02_consumo_kawasaki.py
+++ 02_conceitos_basicos/exercicio_02_consumo_kawasaki.py
@@ -12,3 +12,7 @@ Calcule e imprima o consumo médio da motocicleta (Km/L) formatado com 2 casas d
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+distância_percorrida = float(input("digite a distância percorrida no total"))
+total_de_combustível = float(input("digite o total de combustível gasto"))
+consumo_médio = ((distância_percorrida + total_de_combustível)/2)
+print(f"O consumo médio foi{consumo_médio:.2f}Km/L")
