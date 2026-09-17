c89b4ce165f4353ff12e908297c9de68c872dac5
Gizelebastos <gizeleoliveirabastos867@gmail.com>
2026-09-17 14:50:22 -0300

    Média ponderada.py

--- 02_conceitos_basicos/exercicio_05_media_ponderada.py
+++ 02_conceitos_basicos/exercicio_05_media_ponderada.py
@@ -9,3 +9,12 @@ Calcule e exiba a média final ponderada utilizando apenas operadores aritmétic
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+print(f"Bem vindo aos calculos das notas do curso tecnico")
+nota_um= int (input("digite a primeira nota"))
+nota_dois= int (input("digite a segunda nota"))
+nota_tres= int (input("digite a terceira nota"))
+nota_um_peso = int (nota_um * 2)
+nota_dois_peso = int(nota_dois * 3)
+nota_tres_peso= int(nota_tres * 5)
+média_final= float ((nota_um_peso + nota_dois_peso + nota_tres_peso)/10)
+print (f"a média final do curso tecnico é {média_final : .2f}")
