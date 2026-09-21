f52ee0179152d2d0d5960b94d50856e00f8dd519
Gizelebastos <gizeleoliveirabastos867@gmail.com>
2026-09-21 13:50:48 -0300

    Mão na massa aula 2.py

---
+++ 03_estruturas_de_decisao/Mão na massa 2.py
@@ -0,0 +1,9 @@
+#TODO: Escreva o programa do par ou ímpar aqui
+numero = int(input("Digite um número inteiro: "))
+
+# Escreva a estrutura if / else
+if numero % 2==0:
+    print("numero par")
+else:
+    print("numero impar")
+    print("operação finalizada")
